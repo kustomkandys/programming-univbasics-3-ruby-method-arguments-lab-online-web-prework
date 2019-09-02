fa94ffcc2f90352862f94f3be75ce9a122157cf9
Keri Deemer <kerid18@att.net>
2019-09-02 04:24:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,12 @@ introduction(name) do
  end
  
  introduction_with_language(name, language) do
-   puts "Hi, my name is #{name} and I am learning to program in Ember.js.\n"
+   puts "Hi, my name is #{ name } and I am learning to program in #{ language }."
+end
+
+introduction_with_language_optional(name, language="Ruby") do
+   puts "Hi, my name is #{ name } and I am learning to program in #{ language }."
+end
 
 
 
